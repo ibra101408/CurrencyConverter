3371c6c192265c3512d72c37e3fa2a23e4f3d859
daniilvodenejev <danikvod@gmail.com>
2025-08-20 19:49:43 +0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Daniil Vodenejev on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
